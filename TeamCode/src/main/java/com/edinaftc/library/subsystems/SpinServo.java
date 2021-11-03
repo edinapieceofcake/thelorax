7f0744b23d4aa07ffcdecf6499bd2590f0447e2a
Shishir Ghate <Shishir.Ghate@dowjones.com>
2021-11-03 08:10:30 -0500

    Set the spinner name

--- TeamCode/src/main/java/com/edinaftc/library/subsystems/SpinServo.java
+++ TeamCode/src/main/java/com/edinaftc/library/subsystems/SpinServo.java
@@ -22,7 +22,7 @@ public class SpinServo extends Subsystem {
 
     public SpinServo(HardwareMap map) {
         //What is name of the servo in robot for spin duck
-        continuousServo = map.get(CRServo.class, "");
+        continuousServo = map.get(CRServo.class, "spinner");
     }
 
     @Override
