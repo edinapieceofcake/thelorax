fcaeab21895ef0a37a4cd8db406370e5ee1e1b69
tester1401 <balaram.adusumilli@gmail.com>
2022-01-25 21:29:21 -0600

    Height and speed
    Increased height for turret shared up button, lowered speed for intake/release, increased speed for primary driving.

--- TeamCode/src/main/java/com/edinaftc/library/subsystems/Intake.java
+++ TeamCode/src/main/java/com/edinaftc/library/subsystems/Intake.java
@@ -21,9 +21,9 @@ public class Intake extends Subsystem {
     @Override
     public void update(){
         if (_direction == Direction.Counterclockwise){
-            _motor4321.setPower(.75);
+            _motor4321.setPower(.8);
         } else if (_direction == Direction.Clockwise){
-            _motor4321.setPower(-1);
+            _motor4321.setPower(-.8);
         } else {
             _motor4321.setPower(0);
         }
